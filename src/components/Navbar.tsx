1ad5d2fc2e17b64f2e46b2c42e3ad8a8121ea6c6
Lugman <lugman.s@grootan.com>
2023-06-29 14:31:30 +0530

    Added Request inspect functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
 import { FiGithub } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+
 
 const Navbar = () => {
     return (
-        <nav className="w-full h-16 px-4 bg-neutral-800 fixed top-0">
+        <nav className="w-full h-16 px-4 border-b border-neutral-700 bg-neutral-950 bg-opacity-75 fixed top-0">
             <div className="flex items-center justify-between max-w-5xl mx-auto h-full">
                 <div className="flex items-center gap-1.5">
-                    <Link to="/"><h1 className="text-xl font-medium text-white">Roketo</h1></Link>
+                    <Link to="/" className="flex items-center gap-1">
+                        <img src={Logo} className="w-5 h-5" />
+                        <h1 className="text-lg font-medium text-gray-400">Roketo</h1>
+                    </Link>
                 </div>
                 <Link to="" target="_blank">
-                    <FiGithub className="text-white h-6 w-6" />
+                    <FiGithub className="text-gray-400 hover:text-white duration-150 transition-colors h-6 w-6" />
                 </Link>
             </div>
         </nav>
